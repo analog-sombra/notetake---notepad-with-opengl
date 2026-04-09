af2acc3e147f1e526424250e87710a7774d032db
analog-sombra <analogsombra@gmail.com>
2026-04-09 12:53:26 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <fmt/core.h>
+#include <string>
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <imgui.h>
+
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_internal.h>
+#include <imgui_stdlib.h>
+
+// automatic adjustment of window
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main(int argc, char *argv[])
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        fmt::print("Failed to initialize GLFW");
+        return EXIT_FAILURE;
+    }
+
+    bool opened = true;
+
+    // openGL version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "NoteTake", NULL, NULL);
+    if (window == NULL)
+    {
+        glfwTerminate();
+        fmt::print("Failed to create GLFW window");
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+
+        fmt::print("Failed to initialize GLAD");
+        return EXIT_FAILURE;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+
+    // setup platform/renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // setup style
+    ImGui::StyleColorsDark();
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        glfwPollEvents();
+        // start Imgui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // clear screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // This is a dynamic text, limited by user raw
+        static std::string user_input = "";
+
+        // get the total size of the display
+        ImVec2 fullScreenSize = ImGui::GetIO().DisplaySize;
+        ImGui::SetNextWindowSize(fullScreenSize);
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+
+        // build Imgui ui
+        if (ImGui::Begin("##source1", &opened, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBackground))
+        {
+            ImGui::InputTextMultiline("##input", &user_input, ImGui::GetContentRegionAvail(), ImGuiInputTextFlags_AllowTabInput);
+        }
+        ImGui::End();
+
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // render Imgui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // send new frame to window
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
